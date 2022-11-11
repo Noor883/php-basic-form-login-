f906e5966d676d9290b227b2081cdd24676ea321
Rasheed <2002560477.UPS@htu.edu.jo>
2022-11-11 19:57:20 +0200

    hi .

--- htu-coh9-1/php_forms/index.php
+++ htu-coh9-1/php_forms/index.php
@@ -8,12 +8,7 @@
   </head>
   <body>
     <a href="../">Back</a>
-    <h1>php forms page 2 </h1>
-    <h1>php forms page 2 </h1>
-    <h1>php forms page 2 </h1>
-    <h1>php forms page 2 </h1>
-    <h1>php forms page 2 </h1>
-    <h1>php forms page 2 </h1>
+    <h1>noor</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
